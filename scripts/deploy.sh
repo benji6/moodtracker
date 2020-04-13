d3f8aa1e6ddba17eb6be019bbbd30db5b768cbd8
Ben Hall <benji2357@gmail.com>
2020-04-13 23:09:21 +0100

    feat(infra): add outputs to display useful info about Api Gateway and Cognito resources

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,4 @@
+set -e
+
 aws cloudformation deploy --capabilities CAPABILITY_IAM --stack-name moodtracker --template-file infra/cloudformation.yml
+aws cloudformation describe-stacks --query "Stacks[0].Outputs" --stack-name moodtracker | cat
