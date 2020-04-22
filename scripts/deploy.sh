94f533e9d716b9276676f0c76a2d8c793d302c76
Ben Hall <benji2357@gmail.com>
2020-04-22 19:38:22 +0100

    chore(scripts): better error handling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-set -e
+set -euo pipefail
 
 aws cloudformation deploy --capabilities CAPABILITY_IAM --stack-name moodtracker --template-file infra/cloudformation.yml
 aws cloudformation describe-stacks --query "Stacks[0].Outputs" --stack-name moodtracker | cat
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-set -e
+set -euo pipefail
 
 aws cloudformation validate-template --template-body file://infra/cloudformation.yml | cat &
 pushd client && yarn test && popd
