d2a62e8070b324e1118e348de60413457afe5022
Ben Hall <benji2357@gmail.com>
2020-04-25 16:45:10 +0100

    feat(api): add /moods DELETE

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,15 +1,27 @@
-import { apiUri } from "./constants";
 import { getIdToken } from "./cognito";
 import { Mood } from "./types";
+
+const moodsUrl =
+  "https://0q11376u8l.execute-api.us-east-1.amazonaws.com/prod/moods";
 
 const getAuthorizationHeader = async () => {
   const idToken = await getIdToken();
   return `Bearer ${idToken.getJwtToken()}`;
 };
 
+export const deleteMoods = async (createdAts: string[]): Promise<void> => {
+  const Authorization = await getAuthorizationHeader();
+  const response = await fetch(moodsUrl, {
+    body: JSON.stringify(createdAts),
+    headers: { Authorization, "Content-Type": "application/json" },
+    method: "DELETE",
+  });
+  if (!response.ok) throw Error(String(response.status));
+};
+
 export const getMoods = async (): Promise<Mood[]> => {
   const Authorization = await getAuthorizationHeader();
-  const response = await fetch(`${apiUri}/moods`, {
+  const response = await fetch(moodsUrl, {
     headers: { Authorization },
   });
   if (!response.ok) throw Error(String(response.status));
@@ -18,7 +30,7 @@ export const getMoods = async (): Promise<Mood[]> => {
 
 export const putMoods = async (moods: Mood[]): Promise<void> => {
   const Authorization = await getAuthorizationHeader();
-  const response = await fetch(`${apiUri}/moods`, {
+  const response = await fetch(moodsUrl, {
     body: JSON.stringify(moods),
     headers: { Authorization, "Content-Type": "application/json" },
     method: "PUT",
