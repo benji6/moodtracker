6f05a0bb8f0a295f90e85bb88b57622be75c1cb8
Ben Hall <benji2357@gmail.com>
2020-06-28 13:59:51 +0100

    fix(MoodGraph): renders correctly on resize

--- client/src/components/pages/Home/MoodGraph.tsx
+++ client/src/components/pages/Home/MoodGraph.tsx
@@ -8,19 +8,11 @@ interface IProps {
 }
 
 export default function MoodGraph({ domain, moods }: IProps) {
-  const [width, setWidth] = React.useState(0);
   const domainSpread = domain[1] - domain[0];
   const range = [0, 10];
   const rangeSpread = range[1] - range[0];
   const pointSideLength = 4;
   const height = 200;
-
-  const ref = React.useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    if (!ref.current) return;
-    setWidth(ref.current.clientWidth);
-  }, [ref.current]);
 
   if (!moods.allIds.length) return null;
 
@@ -28,39 +20,42 @@ export default function MoodGraph({ domain, moods }: IProps) {
     <Paper>
       <h2>Mood graph</h2>
       <div
-        ref={ref}
         style={{
-          border: "1px solid var(--e-color-figure)",
-          borderRadius: "var(--e-border-radius-0)",
+          border: "var(--e-border-default)",
           filter: "var(--e-drop-shadow-0)",
           height,
         }}
       >
-        {moods.allIds.map((id) => {
-          const mood = moods.byId[id];
-          const x =
-            ((new Date(id).getTime() - domain[0]) / domainSpread) *
-            (width - pointSideLength);
-          const y =
-            height -
-            pointSideLength -
-            (mood.mood / rangeSpread) * (height - pointSideLength);
+        <div
+          style={{
+            height: `calc(100% - ${pointSideLength}px)`,
+            marginRight: pointSideLength,
+            marginTop: pointSideLength,
+            position: "relative",
+          }}
+        >
+          {moods.allIds.map((id) => {
+            const mood = moods.byId[id];
+            const x = (new Date(id).getTime() - domain[0]) / domainSpread;
+            const y = mood.mood / rangeSpread;
 
-          return (
-            <div
-              key={id}
-              style={{
-                background: "var(--e-color-theme)",
-                borderRadius: "50%",
-                height: pointSideLength,
-                position: "absolute",
-                width: pointSideLength,
-                transform: `translate(${x}px, ${y}px)`,
-              }}
-              title={`Mood: ${mood.mood}`}
-            />
-          );
-        })}
+            return (
+              <div
+                key={id}
+                style={{
+                  background: "var(--e-color-theme)",
+                  borderRadius: "50%",
+                  height: pointSideLength,
+                  position: "absolute",
+                  width: pointSideLength,
+                  left: `${x * 100}%`,
+                  bottom: `${y * 100}%`,
+                }}
+                title={`Mood: ${mood.mood}`}
+              />
+            );
+          })}
+        </div>
       </div>
     </Paper>
   );
