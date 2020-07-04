1701dc320407061d91af664e1e9dea4d4acf29b2
Ben Hall <benji2357@gmail.com>
2020-07-04 12:01:11 +0100

    fix(*): only load notes from storage after user is loaded

--- client/src/components/hooks/useStorage.ts
+++ client/src/components/hooks/useStorage.ts
@@ -6,19 +6,18 @@ export default function useStorage() {
   const dispatch = React.useContext(DispatchContext);
   const state = React.useContext(StateContext);
 
-  React.useEffect(
-    () =>
-      void (async () => {
-        try {
-          const events = await storage.getEvents();
-          if (events)
-            dispatch({ type: "events/loadFromStorage", payload: events });
-        } finally {
-          dispatch({ type: "storage/loaded" });
-        }
-      })(),
-    []
-  );
+  React.useEffect(() => {
+    if (!state.userEmail) return;
+    void (async () => {
+      try {
+        const events = await storage.getEvents();
+        if (events)
+          dispatch({ type: "events/loadFromStorage", payload: events });
+      } finally {
+        dispatch({ type: "storage/loaded" });
+      }
+    })();
+  }, [state.userEmail]);
 
   React.useEffect(() => {
     if (state.isStorageLoading) return;
