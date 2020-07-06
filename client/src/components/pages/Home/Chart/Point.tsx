dd30d94c92ba9b77e8fae87d795ec8d843e76b1d
Ben Hall <benji2357@gmail.com>
2020-07-06 19:15:05 +0100

    feat(Point): color is dependent on value

--- client/src/components/pages/Home/Chart/Point.tsx
+++ client/src/components/pages/Home/Chart/Point.tsx
@@ -1,6 +1,17 @@
+import { interpolatePlasma } from "d3-scale-chromatic";
 import * as React from "react";
 import { POINT_SIZE } from "./constants";
 import { ChartContext } from ".";
+
+let colorCache = new Map();
+
+const getColor = (n: number): string => {
+  const cachedColor = colorCache.get(n);
+  if (cachedColor) return cachedColor;
+  const color = interpolatePlasma(n * 0.75);
+  colorCache.set(n, color);
+  return color;
+};
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   x: number;
@@ -11,16 +22,17 @@ export default function Point({ x, y, ...rest }: Props) {
   const { domain, domainSpread, range, rangeSpread } = React.useContext(
     ChartContext
   );
+  const normalizedY = (y - range[0]) / rangeSpread;
   return (
     <div
       style={{
-        background: "var(--e-color-theme)",
+        background: getColor(normalizedY),
         borderRadius: "50%",
         height: POINT_SIZE,
         position: "absolute",
         width: POINT_SIZE,
         left: `${((x - domain[0]) / domainSpread) * 100}%`,
-        bottom: `${((y - range[0]) / rangeSpread) * 100}%`,
+        bottom: `${normalizedY * 100}%`,
       }}
       {...rest}
     />
