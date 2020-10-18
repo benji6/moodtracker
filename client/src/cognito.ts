08cc8f6e709918030b5a03c741d5ff0a97e4e746
Ben Hall <benji2357@gmail.com>
2020-10-18 21:45:41 +0100

    chore(dependencies): upgrade

--- client/src/cognito.ts
+++ client/src/cognito.ts
@@ -13,12 +13,14 @@ export const getIdToken = (): Promise<CognitoIdToken> =>
   new Promise((resolve, reject) => {
     const currentUser = userPool.getCurrentUser();
     if (!currentUser) return reject(Error("No current user"));
-    currentUser.getSession((err: Error | void, session: CognitoUserSession) => {
-      if (!err) return resolve(session.getIdToken());
-      if (err.message === "User does not exist.") {
-        currentUser.signOut();
-        return reject(Error("No current user"));
+    currentUser.getSession(
+      (err: Error | void, session: CognitoUserSession | null) => {
+        if (!err) return resolve(session!.getIdToken());
+        if (err.message === "User does not exist.") {
+          currentUser.signOut();
+          return reject(Error("No current user"));
+        }
+        reject(err);
       }
-      reject(err);
-    });
+    );
   });
