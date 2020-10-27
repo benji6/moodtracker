ad48fc427ac62f0640ace5a20a90f0b3bb777f28
Ben Hall <benji2357@gmail.com>
2020-10-27 12:19:39 +0000

    feat(MonthlyAverages): add pagination

--- client/src/components/pages/Stats/MonthlyAverages.tsx
+++ client/src/components/pages/Stats/MonthlyAverages.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Paper } from "eri";
+import { Pagination, Paper } from "eri";
 import { mapRight, formatIsoMonthInLocalTimezone } from "../../../utils";
 import MoodCell from "./MoodCell";
 import { monthFormatter } from "../../../formatters";
@@ -7,8 +7,18 @@ import { Link } from "@reach/router";
 import { averageByMonthSelector } from "../../../selectors";
 import { useSelector } from "react-redux";
 
+const MAX_MONTHS_PER_PAGE = 12;
+
 export default function MonthlyAverages() {
   const averageByMonth = useSelector(averageByMonthSelector);
+  const [page, setPage] = React.useState(0);
+
+  const pageCount = Math.ceil(averageByMonth.length / MAX_MONTHS_PER_PAGE);
+  const startIndex = Math.max(
+    0,
+    averageByMonth.length - MAX_MONTHS_PER_PAGE * (page + 1)
+  );
+  const endIndex = averageByMonth.length - MAX_MONTHS_PER_PAGE * page;
 
   return (
     <Paper>
@@ -21,21 +31,25 @@ export default function MonthlyAverages() {
           </tr>
         </thead>
         <tbody>
-          {mapRight(averageByMonth, ([month, averageMood]) => {
-            const monthStr = monthFormatter.format(month);
-            return (
-              <tr key={monthStr}>
-                <td>
-                  <Link to={`months/${formatIsoMonthInLocalTimezone(month)}`}>
-                    {monthStr}
-                  </Link>
-                </td>
-                <MoodCell mood={averageMood} />
-              </tr>
-            );
-          })}
+          {mapRight(
+            averageByMonth.slice(startIndex, endIndex),
+            ([month, averageMood]) => {
+              const monthStr = monthFormatter.format(month);
+              return (
+                <tr key={monthStr}>
+                  <td>
+                    <Link to={`months/${formatIsoMonthInLocalTimezone(month)}`}>
+                      {monthStr}
+                    </Link>
+                  </td>
+                  <MoodCell mood={averageMood} />
+                </tr>
+              );
+            }
+          )}
         </tbody>
       </table>
+      <Pagination onChange={setPage} page={page} pageCount={pageCount} />
     </Paper>
   );
 }
