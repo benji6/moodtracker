f3da7ab2002d67bc08c724a813c92286a1b30152
Ben Hall <benji2357@gmail.com>
2020-12-30 22:50:37 +0000

    fix(EditMood): make sure exploration rows attribute is the same as on the AddMood page

--- client/src/components/pages/EditMood/index.tsx
+++ client/src/components/pages/EditMood/index.tsx
@@ -96,7 +96,7 @@ export default function EditMood({ id }: RouteComponentProps<{ id: string }>) {
             label="Exploration"
             name="exploration"
             optional
-            rows={3}
+            rows={5}
             supportiveText="This is a space to explore how you're feeling, why you're feeling that way and what's going on in your life right now"
           />
           <Button.Group>
