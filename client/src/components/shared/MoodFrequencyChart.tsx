ad1ed38e88b9e7e9e2901b3ae3fe8391c87d225e
Ben Hall <benji2357@gmail.com>
2021-01-01 23:29:33 +0000

    fix(MoodFrequencyChart): change the way y labels are generated so the range is always a multiple of 10

--- client/src/components/shared/MoodFrequencyChart.tsx
+++ client/src/components/shared/MoodFrequencyChart.tsx
@@ -2,29 +2,28 @@ import { Chart } from "eri";
 import * as React from "react";
 import { moodToColor } from "../../utils";
 
-const MOOD_FREQUENCY_CHART_MAX_Y_LABELS = 10;
+// really this is the count minus 1 because it excludes 0
+const Y_LABELS_COUNT = 10;
 
 interface Props {
   data: number[];
 }
 
 export default function MoodFrequencyChart({ data }: Props) {
-  const maxFrequency = Math.max(...data);
+  const maxYLabel =
+    Math.ceil(Math.max(...data) / Y_LABELS_COUNT) * Y_LABELS_COUNT;
 
-  const yLabels: [number, string][] =
-    maxFrequency < MOOD_FREQUENCY_CHART_MAX_Y_LABELS
-      ? [...Array(maxFrequency + 1).keys()].map((y) => [y, String(y)])
-      : [...Array(MOOD_FREQUENCY_CHART_MAX_Y_LABELS).keys()].map((n) => {
-          const y = Math.round(
-            (n / (MOOD_FREQUENCY_CHART_MAX_Y_LABELS - 1)) * maxFrequency
-          );
-          return [y, String(y)];
-        });
+  const yLabels: [number, string][] = [...Array(Y_LABELS_COUNT + 1).keys()].map(
+    (n) => {
+      const y = Math.round((n / Y_LABELS_COUNT) * maxYLabel);
+      return [y, String(y)];
+    }
+  );
 
   return (
     <Chart.BarChart
       aria-label="Chart displaying the frequency at which different moods were recorded"
-      range={[0, maxFrequency]}
+      range={[0, maxYLabel]}
       xAxisTitle="Mood"
       xLabels={data.map((_, i) => i).map(String)}
       yAxisTitle="Count"
