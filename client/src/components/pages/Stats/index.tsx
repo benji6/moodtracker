78e291193e60abc2aa056f386616efbf3b9afbd9
Ben Hall <benji2357@gmail.com>
2021-01-11 22:18:40 +0000

    feat(Stats): add links to this week, this month, this year and explore pages

--- client/src/components/pages/Stats/index.tsx
+++ client/src/components/pages/Stats/index.tsx
@@ -12,6 +12,13 @@ import {
 } from "../../../selectors";
 import { useSelector } from "react-redux";
 import Years from "./Years";
+import {
+  formatIsoDateInLocalTimezone,
+  formatIsoMonthInLocalTimezone,
+  formatIsoYearInLocalTimezone,
+} from "../../../utils";
+import startOfWeek from "date-fns/startOfWeek";
+import { WEEK_OPTIONS } from "../../../formatters";
 
 export default function Stats(_: RouteComponentProps) {
   useRedirectUnauthed();
@@ -27,9 +34,37 @@ export default function Stats(_: RouteComponentProps) {
     );
 
   if (!events.hasLoadedFromServer) return <Spinner />;
+  const now = new Date();
 
   return (
     <Paper.Group>
+      <Paper>
+        <h2>Stats</h2>
+        <ul>
+          <li>
+            <Link
+              to={`/stats/weeks/${formatIsoDateInLocalTimezone(
+                startOfWeek(now, WEEK_OPTIONS)
+              )}`}
+            >
+              This week
+            </Link>
+          </li>
+          <li>
+            <Link to={`/stats/months/${formatIsoMonthInLocalTimezone(now)}`}>
+              This month
+            </Link>
+          </li>
+          <li>
+            <Link to={`/stats/years/${formatIsoYearInLocalTimezone(now)}`}>
+              This year
+            </Link>
+          </li>
+          <li>
+            <Link to="/stats/explore">Explore</Link>
+          </li>
+        </ul>
+      </Paper>
       <Weeks />
       <Months />
       <Years />
