553869185bf88b92a644f669d2791112d93ba782
Ben Hall <benji2357@gmail.com>
2021-01-17 19:17:23 +0000

    docs(TOTAL_USERS): update to latest figure from cognito console

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -17,7 +17,7 @@ export const NETWORK_ERROR_MESSAGE =
 
 // extracted from the cognito console and update manually for now
 // if it ever starts increasing quickly then we can automate this
-export const TOTAL_USERS = 33;
+export const TOTAL_USERS = 34;
 
 const now = Date.now();
 const startOfWeekDate = startOfWeek(now, WEEK_OPTIONS);
