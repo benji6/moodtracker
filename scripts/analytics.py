0c5b33e215775c1f27df8760eced1b50796c609a
Ben Hall <benji2357@gmail.com>
2021-02-10 21:37:41 +0000

    chore(scripts): add breakdown by month to the analytics script

--- scripts/analytics.py
+++ scripts/analytics.py
@@ -1,5 +1,5 @@
 # This script pulls the number of users and the number
-# of events for each day from the database.
+# of events for each day and month from the database.
 # It will not scale in a cost effective manner
 # and is very rudimentary, but suffices to give a basic
 # picture of usage over time for now.
@@ -13,10 +13,8 @@ table = dynamodb.Table('moodtracker_events')
 
 response = table.scan(
   ProjectionExpression='createdAt,userId',
-  ReturnConsumedCapacity='TOTAL'
+  ReturnConsumedCapacity='TOTAL',
 )
-
-print(response['ConsumedCapacity'])
 
 if 'LastEvaluatedKey' in response:
   print('Warning: only 1 page was scanned')
@@ -24,18 +22,32 @@ if 'LastEvaluatedKey' in response:
 events = response['Items']
 events.sort(key=operator.itemgetter('createdAt'))
 
-results = {}
-for event in events:
-  key = event['createdAt'].split('T')[0]
-  stats = results.get(key)
-  if stats:
-    stats['events'] += 1
-    stats['userIds'].add(event['userId'])
-  else:
-    results[key] = {'events': 1, 'userIds': {event['userId']}}
+def compute_breakdown(get_key):
+  results = {}
 
-for k,v in results.items():
-  v['users'] = len(v['userIds'])
-  del v['userIds']
+  for event in events:
+    key = get_key(event['createdAt'])
+    stats = results.get(key)
+    if stats:
+      stats['events'] += 1
+      stats['userIds'].add(event['userId'])
+    else:
+      results[key] = {'events': 1, 'userIds': {event['userId']}}
 
-print(json.dumps(results, indent=2))
+  for k,v in results.items():
+    v['users'] = len(v['userIds'])
+    del v['userIds']
+
+  return results
+
+def get_iso_date_string(date_time_string):
+  return date_time_string[0:10]
+
+def get_iso_month_string(date_time_string):
+  return date_time_string[0:7]
+
+print(json.dumps({
+  'Consumed capacity': response['ConsumedCapacity'],
+  'Breakdown by day': compute_breakdown(get_iso_date_string),
+  'Breakdown by month': compute_breakdown(get_iso_month_string),
+}, indent=2))
