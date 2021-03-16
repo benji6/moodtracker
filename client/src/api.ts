885148245f15cc44d6c3d6008f3d94dff5f2a241
Ben Hall <benji2357@gmail.com>
2021-03-16 20:31:50 +0000

    refactor(client): add fetchWithAuth helper function

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,11 +1,21 @@
 import { getIdToken } from "./cognito";
 import { AppEvent } from "./types";
 
-const eventsUrl = "https://moodtracker.link/api/events";
+const API_URL = "https://moodtracker.link/api";
+const EVENTS_URL = `${API_URL}/events`;
 
-const getAuthorizationHeader = async () => {
+const fetchWithAuth: typeof fetch = async (
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<Response> => {
   const idToken = await getIdToken();
-  return `Bearer ${idToken.getJwtToken()}`;
+  return fetch(input, {
+    ...init,
+    headers: {
+      ...init?.headers,
+      Authorization: `Bearer ${idToken.getJwtToken()}`,
+    },
+  });
 };
 
 export const getEvents = async (
@@ -14,20 +24,17 @@ export const getEvents = async (
   events: AppEvent[];
   nextCursor: string;
 }> => {
-  const Authorization = await getAuthorizationHeader();
-  const response = await fetch(
-    cursor ? `${eventsUrl}/?cursor=${encodeURIComponent(cursor)}` : eventsUrl,
-    { headers: { Authorization } }
+  const response = await fetchWithAuth(
+    cursor ? `${EVENTS_URL}/?cursor=${encodeURIComponent(cursor)}` : EVENTS_URL
   );
   if (!response.ok) throw Error(String(response.status));
   return response.json();
 };
 
 export const postEvents = async (events: AppEvent[]): Promise<void> => {
-  const Authorization = await getAuthorizationHeader();
-  const response = await fetch(eventsUrl, {
+  const response = await fetchWithAuth(EVENTS_URL, {
     body: JSON.stringify(events),
-    headers: { Authorization, "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" },
     method: "POST",
   });
   if (!response.ok) throw Error(String(response.status));
