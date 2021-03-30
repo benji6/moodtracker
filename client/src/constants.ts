cbf1f603cc706fe7a2caa8dbcedd8503e20a0d18
Ben Hall <benji2357@gmail.com>
2021-03-30 19:02:34 +0100

    docs(TOTAL_USERS): update

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -18,7 +18,7 @@ export const NETWORK_ERROR_MESSAGE =
 
 // extracted from cognito and updated manually for now
 // if needed we can look at automating this in the future
-export const TOTAL_USERS = 46;
+export const TOTAL_USERS = 47;
 
 const now = Date.now();
 const startOfWeekDate = startOfWeek(now, WEEK_OPTIONS);
