b8a9242f42e0da5a036452a504a10b65defbee0a
Ben Hall <benji2357@gmail.com>
2021-04-14 19:51:47 +0100

    feat(icons): remove favicon.ico and use SVG icon instead

--- client/scripts/icons.js
+++ client/scripts/icons.js
@@ -30,7 +30,6 @@ favicons(source, configuration, (err, response) => {
         "android-chrome-192x192.png",
         "android-chrome-512x512.png",
         "apple-touch-icon.png",
-        "favicon.ico",
       ].includes(name)
     )
     .map(({ contents, name }) => ({
