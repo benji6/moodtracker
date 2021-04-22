benji6/moodtracker

commit ab22c2388d56e3a541889b81b901a39a270b581e

fix(useUser): fix bug in Firefox private browsing where the UI hangs in a loading state

client/src/components/hooks/useUser.ts
@@ -27,10 +27,8 @@ export default function useUser(): void {
               })
             );
         } catch (e) {
-          if (e.message === "No current user") {
-            dispatch(userSlice.actions.clear());
-            dispatch(eventsSlice.actions.clear());
-          }
+          dispatch(userSlice.actions.clear());
+          dispatch(eventsSlice.actions.clear());
         }
       })(),
     [dispatch]

commit 4a32ed920f92b1aea39a2c8ed7c28ba87c854b6b

fix(useUser): fix try/catch logic
Fixes a suspected regression in ab22c2388d56e3a541889b81b901a39a270b581e which might have broken offline mode

client/src/components/hooks/useUser.ts
@@ -2,7 +2,7 @@ import * as React from "react";
 import { getIdToken } from "../../cognito";
 import storage from "../../storage";
 import { useDispatch } from "react-redux";
-import userSlice from "../../store/userSlice";
+import userSlice, { UserDetails } from "../../store/userSlice";
 import eventsSlice from "../../store/eventsSlice";
 
 export default function useUser(): void {
@@ -11,9 +11,20 @@ export default function useUser(): void {
   React.useEffect(
     () =>
       void (async () => {
+        let storedUser: UserDetails | undefined;
+
         try {
-          const storedUser = await storage.getUser();
-          if (storedUser) dispatch(userSlice.actions.set(storedUser));
+          // Fails in Firefox private browsing
+          storedUser = await storage.getUser();
+        } catch {
+          dispatch(userSlice.actions.clear());
+          dispatch(eventsSlice.actions.clear());
+          return;
+        }
+
+        if (storedUser) dispatch(userSlice.actions.set(storedUser));
+
+        try {
           const idToken = await getIdToken();
           if (
             !storedUser ||
@@ -27,6 +38,7 @@ export default function useUser(): void {
               })
             );
         } catch (e) {
+          if (e.message !== "No current user") return;
           dispatch(userSlice.actions.clear());
           dispatch(eventsSlice.actions.clear());
         }