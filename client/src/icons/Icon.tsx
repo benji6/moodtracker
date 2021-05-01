fb493a58171187f84d6ac415bdd09f751b296538
Ben Hall <benji2357@gmail.com>
2021-05-01 22:30:19 +0100

    feat(icon): update to new icon

--- client/src/icons/Icon.tsx
+++ client/src/icons/Icon.tsx
@@ -4,18 +4,25 @@ export default function MoodTrackerIcon(
   props: React.HtmlHTMLAttributes<SVGElement>
 ) {
   return (
-    <svg
-      {...props}
-      stroke="#000"
-      strokeWidth=".2"
-      viewBox="0 0 10 10"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect height="10" stroke="none" width="10" fill="#fff" />
-      <circle r="1.5" cx="3" cy="3" fill="#2575d0" />
-      <circle r="1.5" cx="3" cy="7" fill="#3bd075" />
-      <circle r="1.5" cx="7" cy="3" fill="#ffa033" />
-      <circle r="1.5" cx="7" cy="7" fill="#ab58ff" />
+    <svg {...props} viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <radialGradient id="gradient">
+          <stop offset="0" stopColor="hsl(195,100%,50%)" />
+          <stop offset=".01" stopColor="hsl(195,100%,50%)" />
+          <stop offset=".15" stopColor="#040606" />
+          <stop offset=".2" stopColor="#040606" />
+          <stop offset=".3" stopColor="hsl(195,100%,50%)" />
+          <stop offset=".4" stopColor="#040606" />
+          <stop offset=".45" stopColor="#040606" />
+          <stop offset=".57" stopColor="hsl(195,100%,50%)" />
+          <stop offset=".69" stopColor="#040606" />
+          <stop offset=".75" stopColor="#040606" />
+          <stop offset=".875" stopColor="hsl(195,100%,50%)" />
+          <stop offset="1" stopColor="#040606" />
+        </radialGradient>
+      </defs>
+      <rect fill="#040606" height="1" stroke="none" width="1" />
+      <circle r=".4" cx=".5" cy=".5" fill="url(#gradient)" />
     </svg>
   );
 }
