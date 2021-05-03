benji6/moodtracker

commit 32495455cf4d887d1b1a8510ed2587f484102ce4

fix(MoodList): can now search by multiple mood tags regardless of order

client/src/components/pages/Home/MoodList.tsx
@@ -103,9 +103,11 @@ export default function MoodList() {
     normalizedDescriptionWordsSelector
   );
 
-  const normalizedFilterDescription = localState.filterDescription
+  const filterDescriptions = localState.filterDescription
     .trim()
-    .toLowerCase();
+    .toLowerCase()
+    .split(/\s+/);
+
   const normalizedFilterExploration = localState.filterExploration
     .trim()
     .toLowerCase();
@@ -115,7 +117,7 @@ export default function MoodList() {
   const shouldFilter = Boolean(
     filterFeatureAvailable &&
       (localState.filterMood !== undefined ||
-        normalizedFilterDescription ||
+        filterDescriptions.length ||
         normalizedFilterExploration)
   );
 
@@ -127,11 +129,15 @@ export default function MoodList() {
           mood.mood !== localState.filterMood
         )
           return false;
-        if (
-          normalizedFilterDescription &&
-          !mood.description?.toLowerCase().includes(normalizedFilterDescription)
-        )
-          return false;
+        if (filterDescriptions.length) {
+          const normalizedMoodDescription = mood.description?.toLowerCase();
+          if (
+            !filterDescriptions.every((description) =>
+              normalizedMoodDescription?.includes(description)
+            )
+          )
+            return false;
+        }
         if (
           normalizedFilterExploration &&
           !mood.exploration?.toLowerCase().includes(normalizedFilterExploration)

commit 7287193717f0cbcb86380a5beaa40d2d6458172a

fix(MoodList): fix bug where some moods are filtered out in error
Bug introduced in 32495455cf4d887d1b1a8510ed2587f484102ce4

client/src/components/pages/Home/MoodList.tsx
@@ -106,7 +106,8 @@ export default function MoodList() {
   const filterDescriptions = localState.filterDescription
     .trim()
     .toLowerCase()
-    .split(/\s+/);
+    .split(/\s+/)
+    .filter(Boolean);
 
   const normalizedFilterExploration = localState.filterExploration
     .trim()