675d1e33e4f37023a66aabfd9420b34d9bb5d583
Ben Hall <benji2357@gmail.com>
2021-05-04 22:34:35 +0100

    fix(icon): bolder lines

--- client/src/icons/Icon/index.tsx
+++ client/src/icons/Icon/index.tsx
@@ -13,46 +13,21 @@ export default function MoodTrackerIcon(
     >
       <defs>
         <radialGradient id="gradient">
-          <stop offset="0" stopColor="var(--m-icon-color-theme)" />
-          <stop offset=".01" stopColor="var(--m-icon-color-theme)" />
-          <stop
-            offset=".15"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
-          <stop
-            offset=".2"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
-          <stop offset=".3" stopColor="var(--m-icon-color-theme)" />
-          <stop
-            offset=".4"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
-          <stop
-            offset=".45"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
-          <stop offset=".57" stopColor="var(--m-icon-color-theme)" />
-          <stop
-            offset=".69"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
-          <stop
-            offset=".75"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
-          <stop offset=".875" stopColor="var(--m-icon-color-theme)" />
-          <stop
-            offset="1"
-            stopColor="var(--m-icon-color-theme)"
-            stopOpacity="0"
-          />
+          <stop offset="0" stopColor="var(--m-icon-color)" />
+          <stop offset=".05" stopColor="var(--m-icon-color)" />
+          <stop offset=".15" stopColor="var(--m-icon-color)" stopOpacity="0" />
+          <stop offset=".2" stopColor="var(--m-icon-color)" stopOpacity="0" />
+          <stop offset=".28" stopColor="var(--m-icon-color)" />
+          <stop offset=".32" stopColor="var(--m-icon-color)" />
+          <stop offset=".4" stopColor="var(--m-icon-color)" stopOpacity="0" />
+          <stop offset=".45" stopColor="var(--m-icon-color)" stopOpacity="0" />
+          <stop offset=".54" stopColor="var(--m-icon-color)" />
+          <stop offset=".6" stopColor="var(--m-icon-color)" />
+          <stop offset=".69" stopColor="var(--m-icon-color)" stopOpacity="0" />
+          <stop offset=".75" stopColor="var(--m-icon-color)" stopOpacity="0" />
+          <stop offset=".84" stopColor="var(--m-icon-color)" />
+          <stop offset=".91" stopColor="var(--m-icon-color)" />
+          <stop offset="1" stopColor="var(--m-icon-color)" stopOpacity="0" />
         </radialGradient>
       </defs>
       <circle r=".4" cx=".5" cy=".5" fill="url(#gradient)" />
