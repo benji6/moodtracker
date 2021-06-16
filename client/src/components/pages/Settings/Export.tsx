ce66f939842a1cd2ba0a1787d5b25a3d6d302c9d
Ben Hall <benji2357@gmail.com>
2021-06-16 21:37:15 +0100

    fix(Export): handle when there is no data to export

--- client/src/components/pages/Settings/Export.tsx
+++ client/src/components/pages/Settings/Export.tsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { denormalizedMoodsSelector } from "../../../selectors";
 import { unparse } from "papaparse";
 import { formatIsoDateInLocalTimezone } from "../../../utils";
+import { useNavigate } from "@reach/router";
 
 const createFilename = (extension: "csv" | "json"): string =>
   `moodtracker-data-export-${formatIsoDateInLocalTimezone(
@@ -13,46 +14,60 @@ const createFilename = (extension: "csv" | "json"): string =>
 
 export default function Export() {
   const denormalizedMoods = useSelector(denormalizedMoodsSelector);
+  const navigate = useNavigate();
 
   return (
     <Paper.Group>
       <Paper>
         <h2>Export data</h2>
-        <p>
-          You can download all your data from here. Choose CSV format if you
-          want to load your data into a spreadsheet and take a closer look.
-        </p>
-        <Button.Group>
-          <Button
-            onClick={() => {
-              const columns: Set<string> = new Set();
-              for (const mood of denormalizedMoods)
-                for (const key of Object.keys(mood)) columns.add(key);
-              saveAs(
-                new Blob(
-                  [unparse(denormalizedMoods, { columns: [...columns] })],
-                  { type: "text/csv" }
-                ),
-                createFilename("csv")
-              );
-            }}
-          >
-            Export as CSV
-          </Button>
-          <Button
-            onClick={() => {
-              saveAs(
-                new Blob([JSON.stringify(denormalizedMoods)], {
-                  type: "application/json",
-                }),
-                createFilename("json")
-              );
-            }}
-            variant="secondary"
-          >
-            Export as JSON
-          </Button>
-        </Button.Group>
+        {denormalizedMoods.length ? (
+          <>
+            <p>
+              You can download all your data from here. Choose CSV format if you
+              want to load your data into a spreadsheet and take a closer look.
+            </p>
+            <Button.Group>
+              <Button
+                onClick={() => {
+                  const columns: Set<string> = new Set();
+                  for (const mood of denormalizedMoods)
+                    for (const key of Object.keys(mood)) columns.add(key);
+                  saveAs(
+                    new Blob(
+                      [unparse(denormalizedMoods, { columns: [...columns] })],
+                      { type: "text/csv" }
+                    ),
+                    createFilename("csv")
+                  );
+                }}
+              >
+                Export as CSV
+              </Button>
+              <Button
+                onClick={() => {
+                  saveAs(
+                    new Blob([JSON.stringify(denormalizedMoods)], {
+                      type: "application/json",
+                    }),
+                    createFilename("json")
+                  );
+                }}
+                variant="secondary"
+              >
+                Export as JSON
+              </Button>
+            </Button.Group>
+          </>
+        ) : (
+          <>
+            <p>No data to export</p>
+            <Button.Group>
+              <Button onClick={() => navigate(`/add`)} type="button">
+                Add your first mood!
+              </Button>
+            </Button.Group>
+          </>
+        )}
       </Paper>
     </Paper.Group>
   );
