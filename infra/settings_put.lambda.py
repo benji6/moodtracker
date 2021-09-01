6fed0ea778da029a9bf58c3149d20a1b19473ade
Ben Hall <benji2357@gmail.com>
2021-09-01 22:27:29 +0100

    feat(api): add settings endpoints

---
+++ infra/settings_put.lambda.py
@@ -0,0 +1,42 @@
+import boto3
+from boto3.dynamodb.conditions import Attr
+import json
+
+HEADERS_NO_CONTENT = {
+  'Access-Control-Allow-Origin': 'http://localhost:1234',
+}
+HEADERS = {
+  **HEADERS_NO_CONTENT,
+  'Content-Type': 'application/json',
+}
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('moodtracker_settings')
+
+def handler(event, context):
+  user_id = event['requestContext']['authorizer']['claims']['sub']
+  settings = json.loads(event['body'])
+  settings['userId'] = user_id
+
+  try:
+    table.put_item(
+      ConditionExpression=Attr('updatedAt').not_exists() | Attr('updatedAt').lt(settings['updatedAt']),
+      Item=settings,
+    )
+    return {
+      'headers': HEADERS_NO_CONTENT,
+      'statusCode': 204,
+    }
+  except dynamodb.meta.client.exceptions.ConditionalCheckFailedException as e:
+    return {
+      'body': json.dumps({'error': 'There is a newer version of settings in the database'}),
+      'headers': HEADERS,
+      'statusCode': 409,
+    }
+  except Exception as e:
+    print(e)
+    return {
+      'body': json.dumps({'error': 'Internal server error'}),
+      'headers': HEADERS,
+      'statusCode': 500,
+    }
