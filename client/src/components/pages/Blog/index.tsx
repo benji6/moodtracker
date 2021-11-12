8cb909c39a11f895c9d80de37da431e87b4be8c9
Ben Hall <benji2357@gmail.com>
2021-11-12 21:36:40 +0000

    fix(Blog): get text from Blog back again (images still broken)

--- client/src/components/pages/Blog/index.tsx
+++ client/src/components/pages/Blog/index.tsx
@@ -1,76 +1,92 @@
 import { RouteComponentProps } from "@reach/router";
-import { Paper, SubHeading } from "eri";
+import { Paper, Spinner, SubHeading } from "eri";
+import { marked } from "marked";
+import { useEffect, useState } from "react";
 import { dateFormatter } from "../../../dateTimeFormatters";
 import Version from "../../shared/Version";
 
 const BLOG_POSTS = {
   "2021-09-24": {
-    __html: String(new URL("2021-09-24/index.md", import.meta.url)),
     title: "New feature - record your location",
+    url: new URL("2021-09-24/index.md", import.meta.url),
   },
   "2021-07-17": {
-    __html: String(new URL("2021-07-17/index.md", import.meta.url)),
     title: "New feature - free meditation",
+    url: new URL("2021-07-17/index.md", import.meta.url),
   },
   "2021-07-15": {
-    __html: String(new URL("2021-07-15/index.md", import.meta.url)),
     title: "New feature - meditation stats page",
+    url: new URL("2021-07-15/index.md", import.meta.url),
   },
   "2021-07-02": {
-    __html: String(new URL("2021-07-02/index.md", import.meta.url)),
     title: "New feature - meditation log",
+    url: new URL("2021-07-02/index.md", import.meta.url),
   },
   "2021-04-25": {
-    __html: String(new URL("2021-04-25/index.md", import.meta.url)),
     title: "New feature - daily statistics",
+    url: new URL("2021-04-25/index.md", import.meta.url),
   },
   "2021-04-18": {
-    __html: String(new URL("2021-04-18/index.md", import.meta.url)),
     title: "New feature - meditation timer",
+    url: new URL("2021-04-18/index.md", import.meta.url),
   },
   "2021-04-04": {
-    __html: String(new URL("2021-04-04/index.md", import.meta.url)),
     title: "New feature - mood gradient visualization",
+    url: new URL("2021-04-04/index.md", import.meta.url),
   },
   "2021-03-18": {
-    __html: String(new URL("2021-03-18/index.md", import.meta.url)),
     title: "New feature - weekly email updates",
+    url: new URL("2021-03-18/index.md", import.meta.url),
   },
   "2021-02-19": {
-    __html: String(new URL("2021-02-19/index.md", import.meta.url)),
     title: "Average mood by hour",
+    url: new URL("2021-02-19/index.md", import.meta.url),
   },
   "2021-02-17": {
-    __html: String(new URL("2021-02-17/index.md", import.meta.url)),
     title: "Improvements to the explore page",
+    url: new URL("2021-02-17/index.md", import.meta.url),
   },
   "2021-01-23": {
-    __html: String(new URL("2021-01-23/index.md", import.meta.url)),
     title: "New feature - search and filter",
+    url: new URL("2021-01-23/index.md", import.meta.url),
   },
   "2021-01-14": {
-    __html: String(new URL("2021-01-14/index.md", import.meta.url)),
     title: "New feature - data export",
+    url: new URL("2021-01-14/index.md", import.meta.url),
   },
   "2021-01-12": {
-    __html: String(new URL("2021-01-12/index.md", import.meta.url)),
     title: "Yearly statistics and other improvements to stats",
+    url: new URL("2021-01-12/index.md", import.meta.url),
   },
   "2021-01-09": {
-    __html: String(new URL("2021-01-09/index.md", import.meta.url)),
     title: "New feature - date controls on explore page",
+    url: new URL("2021-01-09/index.md", import.meta.url),
   },
   "2021-01-01": {
-    __html: String(new URL("2021-01-01/index.md", import.meta.url)),
     title: "New feature - calendar view",
+    url: new URL("2021-01-01/index.md", import.meta.url),
   },
   "2020-12-30": {
-    __html: String(new URL("2020-12-30/index.md", import.meta.url)),
     title: "New feature - exploration",
+    url: new URL("2020-12-30/index.md", import.meta.url),
   },
 };
 
 export default function Blog(_: RouteComponentProps) {
+  const [posts, setPosts] = useState<
+    { __html: string; dateString: string; title: string }[]
+  >([]);
+  useEffect(() => {
+    Promise.all(
+      Object.entries(BLOG_POSTS).map(async ([dateString, { url, title }]) => {
+        const response = await fetch(String(url));
+        const text = await response.text();
+        const __html = marked.parse(text);
+        return { __html, dateString, title };
+      })
+    ).then(setPosts);
+  }, []);
+
   return (
     <Paper.Group>
       <Paper>
@@ -83,24 +99,30 @@ export default function Blog(_: RouteComponentProps) {
         </p>
         <Version />
       </Paper>
-      {Object.entries(BLOG_POSTS).map(([dateString, { __html, title }]) => (
-        <Paper key={dateString}>
-          <article itemScope itemType="http://schema.org/BlogPosting">
-            <h2>
-              {title}
-              <SubHeading>
-                <time
-                  dateTime={dateString}
-                  itemProp="dateCreated datePublished pubdate"
-                >
-                  {dateFormatter.format(new Date(dateString))}
-                </time>
-              </SubHeading>
-            </h2>
-            <div dangerouslySetInnerHTML={{ __html }} />
-          </article>
-        </Paper>
-      ))}
+      {posts.length ? (
+        posts.map(({ __html, dateString, title }) => (
+          <Paper key={dateString}>
+            <article itemScope itemType="http://schema.org/BlogPosting">
+              <h2>
+                {title}
+                <SubHeading>
+                  <time
+                    dateTime={dateString}
+                    itemProp="dateCreated datePublished pubdate"
+                  >
+                    {dateFormatter.format(new Date(dateString))}
+                  </time>
+                </SubHeading>
+              </h2>
+              <div dangerouslySetInnerHTML={{ __html }} />
+            </article>
+          </Paper>
+        ))
+      ) : (
+        <div>
+          <Spinner />
+        </div>
+      )}
     </Paper.Group>
   );
 }
