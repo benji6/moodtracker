7763ee256ec750caad090e07c5ece88652f91404
Ben Hall <benji2357@gmail.com>
2021-11-20 23:27:52 +0000

    perf(usage_get): add database cache

--- infra/usage_get.lambda.py
+++ infra/usage_get.lambda.py
@@ -3,93 +3,124 @@ from boto3.dynamodb.conditions import Attr
 from datetime import datetime, timedelta
 import json
 
+CACHE_KEY = 'usage'
 HEADERS = {'Content-Type': 'application/json'}
+SECONDS_PER_DAY = 86400
 USER_POOL_ID = 'us-east-1_rdB8iu5X4'
 
 cache = {}
 
 cognito_client = boto3.client('cognito-idp')
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('moodtracker_events')
+cache_table = dynamodb.Table('moodtracker_global_cache')
+events_table = dynamodb.Table('moodtracker_events')
 
 def handler(event, context):
   now = datetime.now()
   consumed_capacity_units = 0
   confirmed_users = 0
-  memoryCacheHit = True
+  db_cache_hit = False
+  memory_cache_hit = False
   user_pages=0
 
-  if 'updated_at' not in cache or now - cache.get('updated_at') > timedelta(1):
-    memoryCacheHit = False
+  def log():
+    print({
+      'consumedCapacityUnits': consumed_capacity_units,
+      'dbCacheHit': db_cache_hit,
+      'memoryCacheHit': memory_cache_hit,
+      'userPages': user_pages,
+    })
 
-    try:
-      users_response = cognito_client.list_users(UserPoolId=USER_POOL_ID)
-      users = users_response['Users']
-      user_pages = 1
-      while 'PaginationToken' in users_response:
-        users_response = cognito_client.list_users(
-          PaginationToken=users_response['PaginationToken'],
-          UserPoolId=USER_POOL_ID,
-        )
-        users += users_response['Users']
-        user_pages += 1
-    except Exception as e:
-      print(e)
-      return {
-        'body': json.dumps({'error': 'Internal server error'}),
-        'headers': HEADERS,
-        'statusCode': 500,
-      }
+  if 'expires_at' in cache and now.timestamp() <= cache.get('expires_at'):
+    memory_cache_hit = True
+    log()
+    return cache['data']
 
-    for user in users:
-      if user['Enabled'] and user['UserStatus'] == 'CONFIRMED':
-        confirmed_users += 1
+  try:
+    cache_response = cache_table.get_item(Key={'key': CACHE_KEY})
+    if 'Item' in cache_response:
+      db_cache_hit = True
+      item = cache_response['Item']
+      if item['expiresAt'] < now.timestamp():
+        cache['expires_at'] = item['expiresAt']
+        cache['data'] = json.dumps(item['data'])
+        log()
+        return cache['data']
+  except Exception as e:
+    print(e)
 
-    filter_expression = Attr('createdAt').gt((now - timedelta(30)).isoformat())
+  try:
+    users_response = cognito_client.list_users(UserPoolId=USER_POOL_ID)
+    users = users_response['Users']
+    user_pages = 1
+    while 'PaginationToken' in users_response:
+      users_response = cognito_client.list_users(
+        PaginationToken=users_response['PaginationToken'],
+        UserPoolId=USER_POOL_ID,
+      )
+      users += users_response['Users']
+      user_pages += 1
+  except Exception as e:
+    print(e)
+    return {
+      'body': json.dumps({'error': 'Internal server error'}),
+      'headers': HEADERS,
+      'statusCode': 500,
+    }
 
-    try:
-      events_response = table.scan(
+  for user in users:
+    if user['Enabled'] and user['UserStatus'] == 'CONFIRMED':
+      confirmed_users += 1
+
+  filter_expression = Attr('createdAt').gt((now - timedelta(30)).isoformat())
+
+  try:
+    events_response = events_table.scan(
+      ExpressionAttributeNames={'#t': 'type'},
+      FilterExpression=filter_expression,
+      ProjectionExpression='createdAt,#t,userId',
+      ReturnConsumedCapacity='TOTAL',
+    )
+    events = events_response['Items']
+    consumed_capacity_units = events_response['ConsumedCapacity']['CapacityUnits']
+
+    while 'LastEvaluatedKey' in events_response:
+      events_response = events_table.scan(
+        ExclusiveStartKey=events_response['LastEvaluatedKey'],
         ExpressionAttributeNames={'#t': 'type'},
         FilterExpression=filter_expression,
         ProjectionExpression='createdAt,#t,userId',
         ReturnConsumedCapacity='TOTAL',
       )
-      events = events_response['Items']
-      consumed_capacity_units = events_response['ConsumedCapacity']['CapacityUnits']
-
-      while 'LastEvaluatedKey' in events_response:
-        events_response = table.scan(
-          ExclusiveStartKey=events_response['LastEvaluatedKey'],
-          ExpressionAttributeNames={'#t': 'type'},
-          FilterExpression=filter_expression,
-          ProjectionExpression='createdAt,#t,userId',
-          ReturnConsumedCapacity='TOTAL',
-        )
-        events += events_response['Items']
-        consumed_capacity_units += events_response['ConsumedCapacity']['CapacityUnits']
-    except Exception as e:
-      print(e)
-      return {
-        'body': json.dumps({'error': 'Internal server error'}),
-        'headers': HEADERS,
-        'statusCode': 500,
-      }
-
-    cache['updated_at'] = now
-    cache['response'] = {
-      'body': json.dumps({
-        'confirmedUsers': confirmed_users,
-        'MAUs': len({event['userId'] for event in events}),
-        'WAUs': len({event['userId'] for event in events if datetime.fromisoformat(event['createdAt'][:-1]) > now - timedelta(7)}),
-      }),
+      events += events_response['Items']
+      consumed_capacity_units += events_response['ConsumedCapacity']['CapacityUnits']
+  except Exception as e:
+    print(e)
+    return {
+      'body': json.dumps({'error': 'Internal server error'}),
       'headers': HEADERS,
-      'statusCode': 200,
+      'statusCode': 500,
     }
 
-  print({
-    'consumedCapacityUnits': consumed_capacity_units,
-    'memoryCacheHit': memoryCacheHit,
-    'user_pages': user_pages,
-  })
+  cache['expires_at'] = round(now.timestamp() + SECONDS_PER_DAY)
+  cache['data'] = {
+    'body': json.dumps({
+      'confirmedUsers': confirmed_users,
+      'MAUs': len({event['userId'] for event in events}),
+      'WAUs': len({event['userId'] for event in events if datetime.fromisoformat(event['createdAt'][:-1]) > now - timedelta(7)}),
+    }),
+    'headers': HEADERS,
+    'statusCode': 200,
+  }
 
-  return cache['response']
+  try:
+    cache_table.put_item(Item={
+      'key': CACHE_KEY,
+      'data': cache['data'],
+      'expiresAt': cache['expires_at'],
+    })
+  except Exception as e:
+    print(e)
+
+  log()
+  return cache['data']
