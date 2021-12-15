e81f5a700c84dc9cd87c122cab7e2a3f29718c09
Ben Hall <benji2357@gmail.com>
2021-12-15 20:01:54 +0000

    chore(dependencies): upgrade

--- client/src/numberFormatters.ts
+++ client/src/numberFormatters.ts
@@ -7,8 +7,7 @@ export const minutesFormatter = Intl.NumberFormat(undefined, {
   unit: "minute",
   unitDisplay: "long",
   style: "unit",
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-} as any); // TODO - remove any once types are up-to-date
+});
 
 export const oneDecimalPlaceFormatter = Intl.NumberFormat(undefined, {
   maximumFractionDigits: 1,
