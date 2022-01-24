d4bc1ec1f7bf16c942420ab5956ced7e07d34c28
Ben Hall <benji2357@gmail.com>
2022-01-24 21:59:09 +0000

    chore(dependencies): upgrade all

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
   moduleNameMapper: {
     "\\.css$": "<rootDir>/__mocks__/styleMock.js",
+    "d3-color": "<rootDir>/node_modules/d3-color/dist/d3-color.js",
+    "d3-interpolate":
+      "<rootDir>/node_modules/d3-interpolate/dist/d3-interpolate.js",
   },
   preset: "ts-jest",
   testTimeout: 3e4,
