9adf103d10a2eaf5f66f0a6b14bdde631025a979
Ben Hall <benji2357@gmail.com>
2022-03-19 10:12:18 +0000

    refactor(App): fix TODO

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -12,10 +12,6 @@ import useSettings from "./hooks/useSettings";
 import useGeolocation from "./hooks/useGeolocation";
 import { Link } from "react-router-dom";
 
-// TODO - throws an exception if we don't assign this first
-// Have not figured out the underlying issue
-const QuickNavLink = QuickNav.Link;
-
 interface Props {
   children: React.ReactNode;
 }
@@ -45,18 +41,18 @@ export default function App({ children }: Props) {
       <AddMoodFab hide={!userIsSignedIn} />
       {userIsSignedIn && (
         <QuickNav>
-          <QuickNavLink aria-label="Home" to="/">
+          <QuickNav.Link aria-label="Home" to="/">
             <Icon name="home" size="3" />
-          </QuickNavLink>
-          <QuickNavLink aria-label="Statistics" to="/stats">
+          </QuickNav.Link>
+          <QuickNav.Link aria-label="Statistics" to="/stats">
             <Icon name="chart" size="3" />
-          </QuickNavLink>
-          <QuickNavLink aria-label="Meditate" to="/meditate">
+          </QuickNav.Link>
+          <QuickNav.Link aria-label="Meditate" to="/meditate">
             <Icon name="bell" size="3" />
-          </QuickNavLink>
-          <QuickNavLink aria-label="Add new mood" to="/add">
+          </QuickNav.Link>
+          <QuickNav.Link aria-label="Add new mood" to="/add">
             <Icon name="plus" size="3" />
-          </QuickNavLink>
+          </QuickNav.Link>
         </QuickNav>
       )}
     </>
