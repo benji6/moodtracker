42a06e976ff0ba4d13f883a018f487a2fd0a127f
Ben Hall <benji2357@gmail.com>
2022-03-29 21:28:16 +0100

    refactor(client): add StrictMode component to root

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,5 @@
 import "eri/dist/index.css";
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -6,11 +7,13 @@ import store from "./store";
 import Routes from "./components/Routes";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
   document.getElementById("root")
 );
 
