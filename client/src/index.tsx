7f1d9adc732407004f52acfea2260d29499c6fa2
Ben Hall <benji2357@gmail.com>
2022-05-01 12:38:57 +0100

    chore(dependencies): upgrade

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,19 @@
 import "eri/dist/index.css";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
 import Routes from "./components/Routes";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <Routes />
       </Provider>
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 if (process.env.NODE_ENV === "production" && navigator.serviceWorker)
