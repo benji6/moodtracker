368d71d8f997c3ebf61f45c71d79046aab39c397
Ben Hall <benji2357@gmail.com>
2022-06-19 23:40:12 +0100

    feat(MoodByMonthChart): clicking on a bar navigates to the stats page for that month

--- client/src/components/pages/Stats/Year/MoodByMonthChart.tsx
+++ client/src/components/pages/Stats/Year/MoodByMonthChart.tsx
@@ -1,9 +1,14 @@
 import { Chart } from "eri";
 import { MOOD_INTEGERS, MOOD_RANGE } from "../../../../constants";
-import { formatIsoDateInLocalTimezone, moodToColor } from "../../../../utils";
+import {
+  formatIsoDateInLocalTimezone,
+  formatIsoMonthInLocalTimezone,
+  moodToColor,
+} from "../../../../utils";
 import { useSelector } from "react-redux";
 import { normalizedAveragesByMonthSelector } from "../../../../selectors";
 import { monthNarrowFormatter } from "../../../../dateTimeFormatters";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   months: Date[];
@@ -13,6 +18,7 @@ export default function MoodByMonthChart({ months }: Props) {
   const normalizedAveragesByMonth = useSelector(
     normalizedAveragesByMonthSelector
   );
+  const navigate = useNavigate();
 
   const xLabels: string[] = [];
   const data: (number | undefined)[] = [];
@@ -34,7 +40,15 @@ export default function MoodByMonthChart({ months }: Props) {
     >
       <Chart.YGridLines lines={MOOD_INTEGERS as number[]} />
       <Chart.PlotArea>
-        <Chart.Bars colorFromY={moodToColor} data={data} />
+        <Chart.Bars
+          colorFromY={moodToColor}
+          data={data}
+          onClick={(i) =>
+            navigate(
+              `/stats/months/${formatIsoMonthInLocalTimezone(months[i])}`
+            )
+          }
+        />
       </Chart.PlotArea>
       <Chart.YAxis
         labels={MOOD_INTEGERS.map((mood) => [mood, String(mood)])}
