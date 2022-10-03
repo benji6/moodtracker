087c200999fd35a02fcdcb0aa47a60e3191a48b4
Ben Hall <benji2357@gmail.com>
2022-10-03 15:15:27 +0100

    test(e2e): break mood tests into own file

--- client/e2e/authed.test.ts
+++ client/e2e/authed.test.ts
@@ -1,10 +1,7 @@
-import puppeteer, { HandleFor } from "puppeteer";
+import puppeteer from "puppeteer";
 import { ERRORS } from "../src/constants";
 import { ROOT_DOCUMENT_TITLE, SELECTORS, URLS } from "./constants";
-import { createAndSetUpBrowser, createAndSetUpPage } from "./utils";
-
-const TEST_USER_EMAIL = process.env.MOODTRACKER_TEST_USER_EMAIL!;
-const TEST_USER_PASSWORD = process.env.MOODTRACKER_TEST_USER_PASSWORD!;
+import { createAndSetUpBrowser, createPageAndSignIn, signIn } from "./utils";
 
 const waitForTransitionToComplete = (): Promise<void> =>
   new Promise((resolve) => setTimeout(resolve, 300));
@@ -24,30 +21,9 @@ describe("authed", () => {
     return response;
   };
 
-  const signIn = async () => {
-    const signInLink = (await page.waitForSelector(SELECTORS.signInLink))!;
-    await tapAndNavigate(signInLink);
-
-    const emailInput = (await page.waitForSelector('[type="email"]'))!;
-    await emailInput.type(TEST_USER_EMAIL);
-    const passwordInput = (await page.$('[type="password"]'))!;
-    await passwordInput.type(TEST_USER_PASSWORD);
-
-    await Promise.all([
-      page.waitForNavigation({ timeout: 5e3 }),
-      passwordInput.press("Enter"),
-    ]);
-
-    await page.waitForSelector(SELECTORS.deviceSpecificSettingsDialog);
-    const closeButton = (await page.$(".close-button"))!;
-    await closeButton.tap();
-    await page.waitForSelector(SELECTORS.moodList);
-  };
-
   beforeAll(async () => {
     browser = await createAndSetUpBrowser();
-    page = await createAndSetUpPage(browser);
-    await signIn();
+    page = await createPageAndSignIn(browser);
   });
 
   afterAll(async () => {
@@ -70,7 +46,7 @@ describe("authed", () => {
 
     await waitForTransitionToComplete();
 
-    await signIn();
+    await signIn(page);
   });
 
   test("the user can not access routes which are not available to authenticated users", async () => {
@@ -86,100 +62,6 @@ describe("authed", () => {
     await page.waitForSelector(SELECTORS.statsOverviewPage);
     expect(page.url()).toBe(URLS.statsOverview);
     expect(await page.title()).toBe("MoodTracker - Stats overview");
-  });
-
-  describe("adding a mood", () => {
-    let descriptionInput: puppeteer.ElementHandle<HTMLInputElement>;
-    let submitButton: puppeteer.ElementHandle<HTMLButtonElement>;
-
-    beforeEach(async () => {
-      await page.goto(URLS.add);
-      await page.waitForSelector(SELECTORS.addMoodPage);
-
-      descriptionInput = ((await page.$(
-        SELECTORS.descriptionInput
-      )) as puppeteer.ElementHandle<HTMLInputElement>)!;
-      submitButton = ((await page.$(
-        SELECTORS.addMoodSubmitButton
-      )) as puppeteer.ElementHandle<HTMLButtonElement>)!;
-    });
-
-    test("mood field errors", async () => {
-      await submitButton.evaluate((el) => el.click());
-
-      let errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(1);
-
-      const errorMessage = await errors[0].evaluate((el) => el.textContent);
-      expect(errorMessage).toBe(ERRORS.required);
-
-      const moodRadioButton = (await page.$(
-        SELECTORS.addMoodRadioButton
-      )) as HandleFor<HTMLInputElement>;
-      await moodRadioButton.evaluate((el) => el.click());
-
-      errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(0);
-    });
-
-    test("description field errors", async () => {
-      let errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(0);
-
-      await descriptionInput.type('hello"world', { delay: 10 });
-      await descriptionInput.press("Enter");
-
-      errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(2);
-      const errorMessage = await errors[1].evaluate((el) => el.textContent);
-      expect(errorMessage).toBe(ERRORS.specialCharacters);
-
-      await descriptionInput.click({ clickCount: 3 });
-      await descriptionInput.type("hello world", { delay: 10 });
-      await descriptionInput.press("Enter");
-      errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(1);
-    });
-
-    test("adding a mood with only a mood value", async () => {
-      const MOOD = Math.floor(Math.random() * 11);
-
-      await submitButton.evaluate((el) => el.click());
-
-      let errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(1);
-
-      const errorMessage = await errors[0].evaluate((el) => el.textContent);
-      expect(errorMessage).toBe(ERRORS.required);
-
-      const moodRadioButton = (await page.$(
-        `${SELECTORS.addMoodRadioButton}[value="${MOOD}"]`
-      )) as HandleFor<HTMLInputElement>;
-      await moodRadioButton.evaluate((el) => el.click());
-
-      errors = await page.$$('[data-eri-id="field-error"]');
-      expect(errors).toHaveLength(0);
-
-      const expectedTime = Math.round(Date.now() / 1e3);
-
-      await submitButton.evaluate((el) => el.click());
-      await page.waitForSelector(SELECTORS.moodList);
-
-      expect(page.url()).toBe(URLS.home);
-
-      const moodCardMood = await page.waitForSelector(SELECTORS.moodCardMood);
-      const moodCardMoodText = await moodCardMood!.evaluate(
-        (el) => el.textContent
-      );
-
-      expect(moodCardMoodText).toBe(String(MOOD));
-
-      const moodCardTime = await page.$(SELECTORS.moodCardTime);
-      const moodCardTimeValue = await moodCardTime!.evaluate((el) =>
-        el.getAttribute("data-time")
-      );
-      expect(moodCardTimeValue).toBe(String(expectedTime));
-    });
   });
 
   describe("meditation", () => {
--- client/e2e/utils.ts
+++ client/e2e/utils.ts
@@ -1,5 +1,8 @@
 import puppeteer from "puppeteer";
-import { ROOT_DOCUMENT_TITLE, URLS } from "./constants";
+import { ROOT_DOCUMENT_TITLE, SELECTORS, URLS } from "./constants";
+
+const TEST_USER_EMAIL = process.env.MOODTRACKER_TEST_USER_EMAIL!;
+const TEST_USER_PASSWORD = process.env.MOODTRACKER_TEST_USER_PASSWORD!;
 
 export const createAndSetUpBrowser = (): Promise<puppeteer.Browser> =>
   puppeteer.launch({
@@ -15,4 +18,32 @@ export const createAndSetUpPage = async (
   await page.goto(URLS.origin);
   expect(await page.title()).toBe(ROOT_DOCUMENT_TITLE);
   return page;
+};
+
+export const createPageAndSignIn = async (
+  browser: puppeteer.Browser
+): Promise<puppeteer.Page> => {
+  const page = await createAndSetUpPage(browser);
+  await signIn(page);
+  return page;
+};
+
+export const signIn = async (page: puppeteer.Page): Promise<void> => {
+  const signInLink = (await page.waitForSelector(SELECTORS.signInLink))!;
+  await Promise.all([page.waitForNavigation(), signInLink.tap()]);
+
+  const emailInput = (await page.waitForSelector('[type="email"]'))!;
+  await emailInput.type(TEST_USER_EMAIL);
+  const passwordInput = (await page.$('[type="password"]'))!;
+  await passwordInput.type(TEST_USER_PASSWORD);
+
+  await Promise.all([
+    page.waitForNavigation({ timeout: 5e3 }),
+    passwordInput.press("Enter"),
+  ]);
+
+  await page.waitForSelector(SELECTORS.deviceSpecificSettingsDialog);
+  const closeButton = (await page.$(".close-button"))!;
+  await closeButton.tap();
+  await page.waitForSelector(SELECTORS.moodList);
 };
