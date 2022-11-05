994b41837defb5f601d3402048918e6eed2c4eba
Ben Hall <benji2357@gmail.com>
2022-11-05 11:42:08 +0000

    feat(WeatherIconCard): use different colors depending on weather

--- client/src/components/shared/Location/Weather/WeatherIconGrid/WeatherIconCard/index.tsx
+++ client/src/components/shared/Location/Weather/WeatherIconGrid/WeatherIconCard/index.tsx
@@ -15,22 +15,43 @@ export default function WeatherIconCard({
   id,
   main,
 }: Props) {
-  let iconName: React.ComponentProps<typeof Icon>["name"];
-  if (id < 300) iconName = "lightning";
-  else if (id < 400) iconName = "drizzle";
-  else if (id < 600) iconName = "rain";
-  else if (id < 700) iconName = "snow";
+  let color = "var(--color-balance-more)";
+  let iconName: React.ComponentProps<typeof Icon>["name"] = "cloud";
+
+  if (id < 300) {
+    color = "var(--color-figure-more)";
+    iconName = "lightning";
+  } else if (id < 400) {
+    color = "steelblue";
+    iconName = "drizzle";
+  } else if (id < 600) {
+    color = "blue";
+    iconName = "rain";
+  } else if (id < 700) iconName = "snow";
   else if (id < 800) iconName = "menu";
-  else if (id === 800) iconName = daytime ? "sun" : "moon";
-  else if (id < 803)
-    iconName = daytime ? "partly-cloudy-day" : "partly-cloudy-night";
-  else iconName = "cloud";
+  else if (id === 800) {
+    if (daytime) {
+      color = "orange";
+      iconName = "sun";
+    } else {
+      color = "rebeccapurple";
+      iconName = "moon";
+    }
+  } else if (id < 803) {
+    if (daytime) {
+      color = "orange";
+      iconName = "partly-cloudy-day";
+    } else {
+      color = "rebeccapurple";
+      iconName = "partly-cloudy-night";
+    }
+  }
 
   return (
-    <Card>
+    <Card color={color}>
       <div className="m-weather-icon-card">
         <b>{main}</b>
-        <Icon draw name={iconName} size="4" />
+        <Icon color={color} draw name={iconName} size="4" />
         <small>{capitalizeFirstLetter(description)}</small>
       </div>
     </Card>
