768008901498d5002014e29390970aa4d23b8692
Ben Hall <benji2357@gmail.com>
2022-11-09 16:27:15 +0000

    refactor(bell): assume AudioContext exists

--- client/src/components/pages/Meditate/MeditationTimer/bell.ts
+++ client/src/components/pages/Meditate/MeditationTimer/bell.ts
@@ -2,17 +2,14 @@ const meditationBellUri = String(
   new URL("meditation-bell.mp3", import.meta.url)
 );
 
-const audioContext = typeof AudioContext !== "undefined" && new AudioContext();
-let bufferSource: AudioBufferSourceNode | null = null;
+const audioContext = new AudioContext();
+let bufferSource: AudioBufferSourceNode | undefined;
 
-const bufferPromise =
-  audioContext &&
-  fetch(meditationBellUri)
-    .then((response) => response.arrayBuffer())
-    .then((data) => audioContext.decodeAudioData(data));
+const bufferPromise = fetch(meditationBellUri)
+  .then((response) => response.arrayBuffer())
+  .then((data) => audioContext.decodeAudioData(data));
 
 const start = async () => {
-  if (!audioContext || !bufferPromise) return;
   const buffer = await bufferPromise;
   stop();
   bufferSource = audioContext.createBufferSource();
@@ -26,7 +23,7 @@ const stop = () => {
   if (!bufferSource) return;
   bufferSource.stop();
   bufferSource.disconnect();
-  bufferSource = null;
+  bufferSource = undefined;
 };
 
 export default { start, stop };
