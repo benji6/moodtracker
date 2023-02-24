2852fc642359324ef2d0cca84349461d62d2c3fd
Ben Hall <benji2357@gmail.com>
2023-02-24 06:41:01 +0200

    docs(Version): use formatDistanceToNow

--- client/src/components/shared/Version.tsx
+++ client/src/components/shared/Version.tsx
@@ -1,11 +1,11 @@
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { BUILD_TIME } from "../../constants";
-import { dateFormatter } from "../../formatters/dateTimeFormatters";
 
 export default function Version() {
   return (
     <p>
-      The version you&apos;re currently using was last updated on{" "}
-      <b>{dateFormatter.format(new Date(BUILD_TIME))}</b>.
+      The version you&apos;re currently using was last updated{" "}
+      <b>{formatDistanceToNow(new Date(BUILD_TIME))} ago</b>.
     </p>
   );
 }
