5361a26df71bd36330841b21466eab34730f9ccb
Ben Hall <benji2357@gmail.com>
2024-02-18 21:27:52 +0000

    fix(service-worker): handle directories when compiling list of files to cache

--- client/scripts/injectCacheList.js
+++ client/scripts/injectCacheList.js
@@ -4,8 +4,22 @@ const { BUILD_PATH } = require("./constants");
 
 const ignoreSet = new Set(["index.html", "robots.txt", "service-worker.js"]);
 
+const compileCacheList = async (pathToRead = "", cacheList = []) => {
+  const dirents = await fs.readdir(path.join(BUILD_PATH, pathToRead), {
+    withFileTypes: true,
+  });
+  await Promise.all(
+    dirents.map(async (dirent) => {
+      if (dirent.isFile()) cacheList.push(path.join(pathToRead, dirent.name));
+      else
+        return compileCacheList(path.join(pathToRead, dirent.name), cacheList);
+    }),
+  );
+  return cacheList;
+};
+
 (async () => {
-  const files = await fs.readdir(BUILD_PATH);
+  const files = await compileCacheList();
   const filteredFiles = files.filter(
     (file) => !(ignoreSet.has(file) || file.endsWith(".map")),
   );
