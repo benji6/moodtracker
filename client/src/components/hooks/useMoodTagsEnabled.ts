9e53d021cead71ca5542a0f7877a68d13cdb7df3
Ben Hall <benji2357@gmail.com>
2025-01-23 06:45:02 +0000

    fix(useMoodTagsEnabled): update ENABLED_USER_IDS

--- client/src/components/hooks/useMoodTagsEnabled.ts
+++ client/src/components/hooks/useMoodTagsEnabled.ts
@@ -4,16 +4,11 @@ import userSlice from "../../store/userSlice";
 // User ids of users who have logged a mood that includes a description
 // between 2023-11-18 and 2024-11-19 and within the last 90 days
 const ENABLED_USER_IDS = new Set([
-  "33fbdb2a-ef69-420f-a59b-811532090690",
-  "461425f3-7f85-48e3-94e6-37b276e408fc",
   "6cc24638-9266-44a8-ae4d-f40e92ba631a",
   "7be9f496-cb47-4719-94cc-e6bf2637a556",
-  "9d243188-3a40-4d3d-8004-c77168394925",
-  "9d3a4f25-06c8-4bb7-a088-a514a7865ec4",
   "b1e6254c-9569-4600-b9f9-9ca085624e40",
   "cb4f6e4d-53b6-441b-ba73-876db28e743f",
   "e5635efd-9a99-41a2-a576-b6f5f69ccfbe",
-  "f36b9e7d-4d76-4e82-9108-99b17becf37d",
 ]);
 
 export function useMoodTagsEnabled(): boolean {
