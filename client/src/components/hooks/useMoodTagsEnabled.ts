00cad8730c9d6cb152a4cccba7458c873cb4ecd9
Ben Hall <benji2357@gmail.com>
2025-02-12 06:29:53 +0000

    chore(useMoodTagsEnabled): update ENABLED_USER_IDS

--- client/src/components/hooks/useMoodTagsEnabled.ts
+++ client/src/components/hooks/useMoodTagsEnabled.ts
@@ -6,7 +6,6 @@ import userSlice from "../../store/userSlice";
 const ENABLED_USER_IDS = new Set([
   "6cc24638-9266-44a8-ae4d-f40e92ba631a",
   "7be9f496-cb47-4719-94cc-e6bf2637a556",
-  "b1e6254c-9569-4600-b9f9-9ca085624e40",
   "cb4f6e4d-53b6-441b-ba73-876db28e743f",
   "e5635efd-9a99-41a2-a576-b6f5f69ccfbe",
 ]);
